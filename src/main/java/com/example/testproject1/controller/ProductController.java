3fe547277e0363ece452f2912f145df07ec23c0f
ongguri <rkdtjsdyd321@naver.com>
2023-06-17 15:33:15 +0900

    Spring Boot Validation

--- src/main/java/com/example/testproject1/controller/ProductController.java
+++ src/main/java/com/example/testproject1/controller/ProductController.java
@@ -3,10 +3,14 @@ package com.example.testproject1.controller;
 import com.example.testproject1.data.dto.ProductDto;
 import com.example.testproject1.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/product-api")
@@ -40,14 +44,32 @@ public class ProductController {
 
     // http://localhost:8080/api/v1/product-api/product
     @PostMapping(value = "/product")
-    public ProductDto createProduct(@RequestBody ProductDto productDto) {
+    public ResponseEntity<ProductDto> createProduct(@Valid @RequestBody ProductDto productDto) {
+        // @Valid 어노테이션으로 검증(validation) 활성화
+
+
+        // @Valid 어노테이션으로 검증을 못하는 경우를 위해 아래와 같은 예시로 하는 경우가 있다.
+        // Validation Code Example
+        /*
+        if (productDto.getProductId().equals("") || productDto.getProductId().isEmpty()) {
+            LOGGER.error("[createProduct] failed Response :: productId is Empty");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(productDto);
+        }
+         */
+
         // 동일한 값을 받아서 동일 값을 밷는다. 제대로 DB에 저장이 되었다 싶으면 그 값을 그대로 밷는 역할.
         String productId = productDto.getProductId();
         String productName = productDto.getProductName();
         int productPrice = productDto.getProductPrice();
         int productStock = productDto.getProductStock();
 
-        return productService.saveProduct(productId, productName, productPrice, productStock);
+        ProductDto response = productService.saveProduct(productId, productName, productPrice, productStock);
+
+        LOGGER.info("[createProduct] Response >> productId = {}, productName = {}, productPrice = {}, productStock = {}"
+                , response.getProductId(), response.getProductName(), response.getProductPrice(),
+                response.getProductStock());
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
     }
 
     // http://localhost:8080/api/v1/product-api/product/{productId}
